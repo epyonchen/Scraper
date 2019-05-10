62067b5bbd53ddf0b439bc94cd9a7f1446ea247f
Benson Chen <benson.chen@ap.jll.com>
2019-05-10 17:45:15 +0800

    Rebuild baidu map
    Rebuild baidu map api query structure

--- baidu_api.py
+++ baidu_api.py
@@ -4,98 +4,118 @@ import requests
 import time
 import random
 import gecodeconvert as gc
+import logging
+import re
+import os
+
+class Baidu:
+
+    def __init__(self, api=None):
+
+        if api == 'map':
+            self.base = 'http://api.map.baidu.com/place/v2/search?'#query={}&ak=' + keys.baidu['map']
+        elif api == 'ocr':
+            from aip import AipOcr
+            from PIL import Image
+            self.client = AipOcr(keys.baidu['ocr_id'], keys.baidu['ocr_ak'], keys.baidu['ocr_sk'])
+
+    # def get_token(self, ak=keys.baidu['ocr_ak'], sk=keys.baidu['ocr_sk']):
+    #     query_token = self.base_token.format(ak, sk)
+    #     print(query_token)
+    #     response = requests.get(query_token).json()
+    #
+    #     return response['access_token']
+
+    def ocr_image_process(self, image, path, bin_threshold):
+        img = image.convert('L')
+        img = img.point(lambda p: p > bin_threshold and 255)
+        img.save(path)
+        with open(path, 'rb') as img_file:
+            return img_file.read()
+
+    def ocr_api_call(self, image, path, bin_threshold=100, **kwargs):
+        bin_image = self.ocr_image_process(image, path, bin_threshold)
+        result = self.client.basicGeneral(bin_image, kwargs)
+        if ('words_result_num' in result.keys()) and result['words_result_num'] > 0:
+            return result
+        else:
+            return None
+
+    # Call map api, refer parameter to http://lbsyun.baidu.com/index.php?title=webapi/guide/webservice-placeapi
+    def map_api_call(self, baidu_key, location_input=None, **kwargs):
+        query = self.base
+        for key, value in kwargs.items():
+            query = query + '&' + key + '=' + str(value)
+        query = query + '&ak=' + baidu_key
+
+        # Request api
+        response = requests.get(query).json()
+        time.sleep(random.randint(1, 2))
+
+        # Validate response
+        if (response['status'] != 0) or response['total'] < 1:
+            return None
+        else:
+            one_call = list()
+            for record in response['results']:
+                mapit = self.geocode_convert(record['location']['lat'], record['location']['lng'])
+                record['MapITlat'] = mapit[1]
+                record['MapITlon'] = mapit[0]
+                record = self.get_nested_value(record)
+                record['keyword'] = kwargs['query']
+
+                # inflag = self.validate_in(store, location_input)
+                # if inflag:
+                #     df = df.append(store, ignore_index=True)
+                # else:
+                #     continue
+
+                one_call.append(record)
+
+            return one_call
 
 
-def search_location_api_call(keyword_query, key, **kwargs):
-    query = 'http://api.map.baidu.com/place/v2/search?query=' + keyword_query
-    for arg in kwargs.keys():
-        query = query + '&' + arg + '=' + str(kwargs[arg])
-    query = query + '&ak=' + key
-    print(query)
-    response = requests.get(query)
-    response = response.json()
-    time.sleep(random.randint(1, 2))
-    return response
+    def get_nested_value(self, record):
+        new_record = record.copy()
+        for key, value in record.items():
+            if isinstance(value, dict):
+                inner_dict = self.get_nested_value(value)
+                new_record.update(inner_dict)
+                del new_record[key]
+            else:
+                continue
+        return new_record
 
+    # Query with keywords list and cities list
+    def search_location(self, keyword_inputs, city_inputs, location_input, **kwargs):
+        df = pd.DataFrame()
 
-def search_location(keyword_inputs, city_inputs, location_input):
-    df = pd.DataFrame(columns=['uid', 'keyword', 'name', 'province', 'city', 'area', 'address', 'lat', 'lon', 'tag', 'type', 'overall_rating', 'comment_num', 'MapITlat', 'MapITlon'])
+        for city in city_inputs:
+            for keyword in keyword_inputs:
+                page = 1
+                print('df size:', len(df))
+                while page > 0:
+                    api_parameter = kwargs.copy()
+                    api_parameter.update({'query':keyword, 'region':city, 'page_num':(page-1)})
+                    one_call = self.map_api_call(keys.baidu['map'], location_input, **api_parameter)
+                    if one_call is not None:
+                        df.append(one_call, ignore_index=True)
+        return df
 
-    for city in city_inputs:
-        for keyword in keyword_inputs:
-            page = 1
-            print('df size:', len(df))
-            while page > 0:
-                response = search_location_api_call(keyword, keys.baidu, tag='美食' ,region=city, city_limit=0, scope=2, page_size=20, page_num=(page-1), output='json')
-                # response = search_location_api_call(keyword, keys.baidu, location='23.123892,113.332952', radius_limit='false', scope=2, radius=500, page_size=20, page_num=(page-1), output='json')
-                if 'total' not in response.keys() or response['total'] < 1 or response['results'] == []:
-                    page = 0
-                    break
-                else:
-                    try:
-                        page += 1
-                        for store in response['results']:
-                            store['keyword'] = keyword
-                            store['lat'] = store['location']['lat']
-                            store['lon'] = store['location']['lng']
-                            mapit = geocode_convert(store['location']['lat'], store['location']['lng'])
-                            store['MapITlat'] = mapit[1]
-                            store['MapITlon'] = mapit[0]
-                            if 'detail_info' in store.keys():
-                                if 'overall_rating' in store['detail_info'].keys():
-                                    store['overall_rating'] = store['detail_info']['overall_rating']
-                                else:
-                                    store['overall_rating'] = None
-                                if 'comment_num' in store['detail_info'].keys():
-                                    store['comment_num'] = store['detail_info']['comment_num']
-                                else:
-                                    store['comment_num'] = None
-                                if 'tag' in store['detail_info'].keys():
-                                    store['tag'] = store['detail_info']['tag']
-                                else:
-                                    store['tag'] = None
-                                if 'type' in store['detail_info'].keys():
-                                    store['type'] = store['detail_info']['type']
-                                else:
-                                    store['type'] = None
-                            # else:
-                            #     store['overall_rating'] = None
-                            #     store['comment_num'] = None
-                            #     store['tag'] = None
-                            #     store['type'] = None
+    # Convert from Baidu to
+    def geocode_convert(self, lat, lon):
 
-                            inflag = validate_in(store, location_input)
-                            if inflag:
-                                df = df.append(store, ignore_index=True)
-                            else:
-                                continue
-                    except:
-                        print('No match.')
-                        break
+        return gc.bd09_to_wgs84(lon, lat)
 
-    return df
-
-
-# Convert from Baidu to
-def geocode_convert(lat, lon):
-
-    return gc.bd09_to_wgs84(lon, lat)
-
-
-def validate_in(store, location_in):
-    for loc in location_in:
-        if (loc in store['address']) or (loc in store['name']):
-            return True
-
-    return False
+    def validate_in(self, record, location_in):
+        for loc in location_in:
+            if (loc in record['address']) or (loc in record['name']):
+                return True
+        return False
 
 
 if __name__ == '__main__':
-    # city_list = ['广州', '深圳', '佛山', '珠海', '东莞', '惠州', '肇庆', '中山']
-    location_in = ['k11', '周大福', '珠江东路6号']
-    keyword_list = ['k11']
-    # keyword_list = pd.read_excel(r'C:\Users\Benson.Chen\Desktop\Scraper\Archive\Project list for TDIM.xlsx', sheet_name='Sheet1')
-    # keyword_list = keyword_list['keyword'].tolist()
-    city_list = ['广州']
-    location_list = search_location(keyword_list, city_list, location_in)
-    location_list.to_excel(r'C:\Users\Benson.Chen\Desktop\Scraper\Result\CAA_k11_search3.xlsx', index=False, header=True, columns=list(location_list), sheet_name='Baidu Api')
+
+    baidu = Baidu()
+    result = baidu.ocr_api_call(language_type='ENG', detect_direction='false')
+    print(result)
