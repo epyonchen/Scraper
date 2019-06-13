72510421d528db509dfea4f6a199944cbb093d98
Benson Chen <benson.chen@ap.jll.com>
2019-06-13 15:53:50 +0800

    Rebuild Mytophome
    Mytophome scraping

--- mytophome.py
+++ mytophome.py
@@ -4,87 +4,111 @@ Created on Sun Jan 15th 2019
 @author: Benson.Chen benson.chen@ap.jll.com
 """
 
-import requests
-from bs4 import BeautifulSoup
 import random
 import time
-import pandas as pd
-from pagemanipulate import pm
 import re
-import datetime
 import db
 import keys
+import requests
+import json
+import utility_email as em
+from utility_commons import *
+from scrapers import TwoStepScraper
+
+SITE = 'Mytophome'
+TABLENAME = 'Scrapy_Mytophome'
+LOG_PATH = LOG_DIR + '\\' + SITE + '.log'
+
+logger = getLogger(SITE)
+_data = {
+    'cityId': '20',
+    'crmDicId': '',
+    'saleType': 'R',
+    'buildArea': '0-50000',
+    'page': '1',
+    'pagesize': '1000'
+}
+
+class MyTopHome(TwoStepScraper):
+    def __init__(self, city):
+        TwoStepScraper.__init__(self, city)
+        self.search_base = 'http://{}.mytophome.com'.format(city)
+        self.search_url = '/estateList/rent/X_0_0_0_0_0_0_0_0.html?orderBy=&isAsc=D&page={}'
 
 
-def office_scraper(city, page):
-    search_base = 'http://{}.mytophome.com'.format(city)
-    search_url = search_base + '/estateList/rent/X_0_0_0_0_0_0_0_0.html?orderBy=&isAsc=D&page={}'.format(str(page))
-    search_response = requests.get(search_url)
-    search_soup = BeautifulSoup(search_response.text, 'lxml')
-    search_result_page = pd.DataFrame()
+    # Get items in one page
+    def get_item_list(self, cityname, pagenum):
+        list_link = self.search_url.format(pagenum)
+        list_soup = self.search(self.search_base + list_link)
+        item_list = list_soup.find_all('div', attrs={'class': 'fyliright'})
+        return item_list
 
-    property_list = search_soup.find_all('div', attrs={'class': 'fyliright'})
-    if len(property_list) < 1:
-        return False
-    for property in property_list:
+    def get_item_detail(self, item):
 
         time.sleep(random.randint(0, 1) / 5.0)
+        item_link = item.h3.a['href']
+        try:
+            global _data
+            _data['crmDicId'] = re.compile(r'\d+').search(item_link).group(0)
+        except Exception as e:
+            logger.exception(e)
+            return False
+        item_name = item.h3.a.text
+        logger.info('Building Name: {}'.format(item_name))
+        item_detail_list = []
+        item_details = requests.post('http://api.mytophome.com/officeService/getOfficeByEstate.do', data=_data)
+        item_details = json.loads(item_details.text[1:-1])
+        if item_details[0]['statusCode'] == 1:
+            item_detail_list += item_details[0]['estatePropList']
 
-        property_url = property.h3.a['href']
-        property_id = re.findall(r'\d+', property_url)[0]
-        property_name = property.h3.a.text
-        property_location = property.find('div', attrs={'class': 'fylifont01'})
-        property_area = property_location.span.text
-        property_address = property_location.text.replace(property_area, '').strip()
-        property_area = property_area.replace('|', '').strip()
-        print(property_name)
+        return item_detail_list
 
-        webpage = pm(property_url)
-        tab = 1
-        while webpage.click('//li[@id="RPx{}"]/a'.format(tab)):
-            search_soup = BeautifulSoup(webpage.soup, 'lxml')
-            office_list = search_soup.find_all('div', attrs={'class': 'fyliright'})
-            for row in office_list:
-                office = dict()
-                office['Office'] = row.h3.a.text
-                office['Office_ID'] = re.findall(r'\d+', row.h3.a['href'])[0]
-                details = row.find('div', attrs={'class': 'fylifont01'}).find_all('span')
-                office['物业费'] = details[0].text
-                office['面积'] = details[1].text
-                office['单价'] = details[2].text
-                office['月租'] = row.find('div', attrs={'class': 'fylirab'}).p.span.text
-                office['近90天带看'] = row.find('div', attrs={'class': 'fylirab02'}).p.text
-                office['Property_ID'] = property_id
-                office['property'] = property_name
-                office['地区'] = property_area
-                office['地址'] = property_address
-                search_result_page = search_result_page.append(office, ignore_index=True, sort=False)
-            tab += 1
-        webpage.close()
 
-    print('Page {} Done.'.format(str(page)))
-    return search_result_page
+        # browser.get(item_link)
+        # tab_soup = BeautifulSoup(browser.soup, 'lxml')
+        # tabs = len(tab_soup.find_all('li', attrs={'id': re.compile('RPx\d+')}))
+        #
+        # for tab in range(1, tabs):
+        #     browser.click('//li[@id="RPx{}"]/a'.format(tab))
+        #     tab_soup = BeautifulSoup(browser.soup, 'lxml')
+        #     detail_list = tab_soup.find_all('div', attrs={'class': 'fyliright'})
+        #
+        #     for row in detail_list:
+        #         item_detail = dict()
+        #         try:
+        #             item_detail['Source_ID'] = self.city + '_' + re.compile(r'\d+').search(row.h3.a['href']).group(0)
+        #             infos = row.find('div', attrs={'class': 'fylifont01'}).find_all('span')
+        #             item_detail['物业费'] = infos[0].text
+        #             item_detail['面积(元/平方米/月)'] = infos[1].text
+        #             item_detail['租金(元/平方米/月)'] = infos[2].text
+        #             item_detail['近90天带看'] = row.find('div', attrs={'class': 'fylirab02'}).p.text
+        #         except Exception as e:
+        #             logger.exception(e)
+        #             # item_detail_list.append(item_detail)
+        #             continue
+        #         item_detail['Property_ID'] = item_id
+        #         item_detail['Property_Name'] = item_name
+        #         item_detail['Name'] = row.h3.a.text
+        #         item_detail_list.append(item_detail)
+        #     tab += 1
+        #     if not browser.exist('//li[@id="RPx{}"]/a'.format(tab)):
+        #         break
+
+        return item_detail_list
+
+    def format_df(self):
+            self.df['estateId'] = self.city + '_' + self.df['estateId']
+            self.df['id'] = self.city + '_' + self.df['id']
 
 
 if __name__ == '__main__':
 
-    site = 'Mytophome'
-    date = str(datetime.date.today())
     cities = ['gz']
-    scrapydb = db.Mssql(keys.dbconfig)
+    with db.Mssql(keys.dbconfig) as scrapydb:
 
-    for city in cities:
+        one_city_df, start, end = MyTopHome.run(city='gz', from_page=0, step=20)
+        scrapydb.upload(one_city_df, TABLENAME, start=start, end=str(int(end) % 20), timestamp=TIMESTAMP, source=SITE, city='gz')
 
-        onecity = pd.DataFrame()
-        page = 0
-        while True:
-            one_page = office_scraper(city, page)
-            if one_page is False:
-                break
-            else:
-                onecity = onecity.append(one_page, ignore_index=True, sort=False)
-                page += 20
-
-        onecity.to_excel(r'C:\Users\Benson.Chen\Desktop\Scraper\Result\{}_{}_{}.xlsx'.format(site, city, date), sheet_name='{} {}'.format(site, city), index=False)
-        scrapydb.upload(onecity, 'Scrapy_{}'.format(site), timestamp=date, source=site,  city=city)
-        scrapydb.close()
+    scrapyemail = em.Email()
+    scrapyemail.send(SITE, 'Done', LOG_PATH)
+    scrapyemail.close()
