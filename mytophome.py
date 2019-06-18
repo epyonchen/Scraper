6351286d3f5d1e2b13205551a0275b713e372846
Benson Chen <benson.chen@ap.jll.com>
2019-06-18 11:56:15 +0800

    Update mytophome.py
    format id and detail id

--- mytophome.py
+++ mytophome.py
@@ -35,7 +35,6 @@ class MyTopHome(TwoStepScraper):
         self.search_base = 'http://{}.mytophome.com'.format(city)
         self.search_url = '/estateList/rent/X_0_0_0_0_0_0_0_0.html?orderBy=&isAsc=D&page={}'
 
-
     # Get items in one page
     def get_item_list(self, cityname, pagenum):
         list_link = self.search_url.format(pagenum)
@@ -43,6 +42,7 @@ class MyTopHome(TwoStepScraper):
         item_list = list_soup.find_all('div', attrs={'class': 'fyliright'})
         return item_list
 
+    # Get item details
     def get_item_detail(self, item):
 
         time.sleep(random.randint(0, 1) / 5.0)
@@ -63,42 +63,9 @@ class MyTopHome(TwoStepScraper):
 
         return item_detail_list
 
-
-        # browser.get(item_link)
-        # tab_soup = BeautifulSoup(browser.soup, 'lxml')
-        # tabs = len(tab_soup.find_all('li', attrs={'id': re.compile('RPx\d+')}))
-        #
-        # for tab in range(1, tabs):
-        #     browser.click('//li[@id="RPx{}"]/a'.format(tab))
-        #     tab_soup = BeautifulSoup(browser.soup, 'lxml')
-        #     detail_list = tab_soup.find_all('div', attrs={'class': 'fyliright'})
-        #
-        #     for row in detail_list:
-        #         item_detail = dict()
-        #         try:
-        #             item_detail['Source_ID'] = self.city + '_' + re.compile(r'\d+').search(row.h3.a['href']).group(0)
-        #             infos = row.find('div', attrs={'class': 'fylifont01'}).find_all('span')
-        #             item_detail['物业费'] = infos[0].text
-        #             item_detail['面积(元/平方米/月)'] = infos[1].text
-        #             item_detail['租金(元/平方米/月)'] = infos[2].text
-        #             item_detail['近90天带看'] = row.find('div', attrs={'class': 'fylirab02'}).p.text
-        #         except Exception as e:
-        #             logger.exception(e)
-        #             # item_detail_list.append(item_detail)
-        #             continue
-        #         item_detail['Property_ID'] = item_id
-        #         item_detail['Property_Name'] = item_name
-        #         item_detail['Name'] = row.h3.a.text
-        #         item_detail_list.append(item_detail)
-        #     tab += 1
-        #     if not browser.exist('//li[@id="RPx{}"]/a'.format(tab)):
-        #         break
-
-        return item_detail_list
-
     def format_df(self):
-            self.df['estateId'] = self.city + '_' + self.df['estateId']
-            self.df['id'] = self.city + '_' + self.df['id']
+            self.df['estateId'] = self.df['estateId'].apply(lambda x: self.city + '_' + x, axis=1)
+            self.df['id'] = self.df['id'].apply(lambda x: self.city + '_' + x, axis=1)
 
 
 if __name__ == '__main__':
