3bd8f818a6fc9f28260aa5010a2d598bf413f5b4
Benson Chen <benson.chen@ap.jll.com>
2019-06-18 14:18:45 +0800

    Bug fixed

--- scrapers.py
+++ scrapers.py
@@ -65,10 +65,11 @@ class TwoStepScraper:
 
     # Query one city
     @classmethod
-    def run(cls, from_page=1, to_page=None, city=None):
+    def run(cls, from_page=1, to_page=None, city=None, step=1):
         page = from_page
         one_city = cls(city)
-        item_load_list = []
+        item_info_load = []
+        item_detail_load = []
 
         if city is None:
             logger.exception('City is missing.')
@@ -81,37 +82,44 @@ class TwoStepScraper:
 
             # Get items in one page
             item_list = one_city.get_item_list(city, page)
-            page += 1
+            page += step
 
             # If item_list is empty, stop query
             if not bool(item_list):
                 logger.info('Page {} is empty. Stop this job.'.format(page - 1))
-                logger.info('Total {} records, {} pages.'.format(str(len(item_load_list)), str(page - from_page)))
-                if item_load_list != []:
-                    one_city.df = one_city.df.append(item_load_list, ignore_index=True, sort=False)
-                return one_city.df, str(from_page), str(page - 1)
+                logger.info('Total {} records, {} pages.'.format(str(len(item_detail_load)), str(page - from_page)))
+                if item_detail_load != []:
+                    one_city.df = one_city.df.append(item_detail_load, ignore_index=True, sort=False)
+                return one_city, str(from_page), str(page - 1)
             else:
                 # Go through items in one page
                 for item in item_list:
                     # Get detail list of one item
                     item_detail_list = one_city.get_item_detail(item)
-                    if item_detail_list is None:
+                    if not item_detail_list:
                         continue
                     else:
-                        item_load_list += item_detail_list
+                        item_detail_load += item_detail_list
 
-        logger.info('Total {} records, {} pages.'.format(str(len(item_load_list)), str(page - from_page)))
+        logger.info('Total {} records, {} pages.'.format(str(len(item_detail_load)), str(page - from_page)))
 
-        if bool(item_load_list):
-            one_city.df = one_city.df.append(item_load_list, ignore_index=True, sort=False)
+        if bool(item_detail_load):
+            one_city.df = one_city.df.append(item_detail_load, ignore_index=True, sort=False)
+        if bool(item_info_load):
+            one_city.info = one_city.info.append(item_info_load, ignore_index=True, sort=False)
         one_city.df = one_city.format_df()
-        return one_city.df, str(from_page), str(page - 1)
+        return one_city, str(from_page), str(page - 1)
 
     # Get item list in one page for one city
     def get_item_list(self, cityname, pagenum):
         item_list = pagenum
         return item_list
-
+    
+    # Get item information
+    def get_item_info(self):
+        item_info = None
+        return item_info
+        
     # Get detail of one item
     def get_item_detail(self, item):
         item_detail = item
