3255253621417076ebd8d52a210744fff42082da
benson.chen <benson.chen@ap.jll.com>
2020-11-24 15:31:21 +0800

    Add function get_df)col_size to get df's size dict. Format parameters wording.

--- utility_commons.py
+++ utility_commons.py
@@ -7,6 +7,7 @@ Created on Thur May 16th 2019
 
 import os
 import sys
+import re
 import datetime
 import pandas as pd
 import logging
@@ -25,6 +26,13 @@ PATH = {
     'PIC_DIR': __TARGET_DIR + r'\Vcode',
     'FILE_DIR': __TARGET_DIR + r'\Result',
     'LOG_DIR': __TARGET_DIR + r'\Log',
+
+}
+
+# database
+DB = {
+    'MAX_COL_SIZE': 250,
+    'DEFAULT_COL_SIZE': 50,
     'LOG_TABLE_NAME': 'Scrapy_Logs',
 }
 
@@ -60,17 +68,17 @@ def get_nested_value(record):
 
 
 # Convert  excel to dataframe
-def excel_to_df(filename, sheet_name=None, path=None):
+def excel_to_df(file_name, sheet_name=None, path=None):
     folder_dir = path if path else PATH['TARGET_DIR']
     para = {'sort': False, 'dtype': str}
     if sheet_name:
         para.update({'sheet_name': sheet_name})
     try:
-        df = pd.read_excel(folder_dir + r'\{}.xlsx'.format(filename), **para)
+        df = pd.read_excel(folder_dir + r'\{}.xlsx'.format(file_name), **para)
     except:
-        logging.info('{}.xlsx does not exist, try {}.xls'.format(filename, filename))
+        logging.info('{}.xlsx does not exist, try {}.xls'.format(file_name, file_name))
         try:
-            df = pd.read_excel(folder_dir + r'\{}.xls'.format(filename), **para)
+            df = pd.read_excel(folder_dir + r'\{}.xls'.format(file_name), **para)
         except Exception:
             logging.exception('Fail to import excel to df')
             return None
@@ -79,8 +87,8 @@ def excel_to_df(filename, sheet_name=None, path=None):
 
 
 # Convert dataframe to excel
-def df_to_excel(df, filename, sheet_name='Results', path=None):
-    file_path = (path if path else PATH['TARGET_DIR']) + r'\{}.xlsx'.format(filename)
+def df_to_excel(df, file_name, sheet_name='Results', path=None):
+    file_path = (path if path else PATH['TARGET_DIR']) + r'\{}.xlsx'.format(file_name)
     try:
         if os.path.exists(file_path):
             with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
@@ -91,9 +99,33 @@ def df_to_excel(df, filename, sheet_name='Results', path=None):
         else:
             df.to_excel(file_path, index=False, header=True, sheet_name=sheet_name)
     except Exception:
-        logging.exception('Fail to export {} to excel.'.format(filename))
+        logging.exception('Fail to export {} to excel.'.format(file_name))
         return None
 
     return True
 
 
+# Return dict of given df's columns' size
+def get_df_col_size(df):
+    col_dict = dict(
+        (col, df[col].astype(str).apply(lambda x: len(x) if x is not None else 0).max()) for col in df.columns.values
+    )
+    return col_dict
+
+
+# Format string length of columns in dataframe
+def chunksize_df_col_size(df, max_len=DB['MAX_COL_SIZE'], inplace=False):
+    df_copy = df.copy() if not inplace else df
+    col_dict = get_df_col_size(df_copy)
+    for k, v in col_dict.items():
+        if v > max_len:
+            df_copy[k] = df_copy[k].astype(str).str.slice(0, max_len)
+    return df_copy
+
+
+# Get current job name
+def get_job_name():
+    module = sys.modules['__main__']
+    name_pattern = re.compile(r'\w+\.py')
+    name = re.search(name_pattern, str(module.__file__)).group(0)
+    return name.replace(r'.py', '')
