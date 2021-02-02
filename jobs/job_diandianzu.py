ab7af9add6d7d5a67dc793e0373739c0660c9e9b
benson.chen <benson.chen@ap.jll.com>
2021-02-02 12:04:36 +0800

    Key tables details to DB dict, refine existing log checking

--- jobs/job_diandianzu.py
+++ jobs/job_diandianzu.py
@@ -8,34 +8,31 @@ Created on Dec 7th 2020
 import utils.utility_email as em
 from handlers.diandianzu import Diandianzu
 from handlers.db import Mssql, get_sql_list
-from utils.utility_commons import TIME, DB, PATH
-from utils.utility_log import get_job_name
+from utils.utility_commons import TIME, PATH, DB
 import keys
 
-SITE = get_job_name()
-PATH['DETAIL_TABLE'] = 'Scrapy_' + SITE
-PATH['INFO_TABLE'] = 'Scrapy_' + SITE + '_Info'
+SITE = 'diandianzu'
+DB['DETAIL_TABLE'] = 'Scrapy_' + SITE
+DB['INFO_TABLE'] = 'Scrapy_' + SITE + '_Info'
 PATH['LOG_PATH'] = PATH['LOG_DIR'] + '\\' + SITE + '.log'
-cities = ['gz', 'sz', 'sh', 'bj', 'cd']
 
 with Mssql(config=keys.dbconfig_mkt) as exist_db:
-    con_city = get_sql_list(cities)
-    condition = '[Timestamp] >= {0} AND [Entity] IN {1} AND [Source] = {2}'.\
-        format(get_sql_list(TIME['TODAY']), get_sql_list(cities), get_sql_list(SITE))
-    existing_cities = exist_db.select(table_name=DB['LOG_TABLE_NAME'], condition=condition)
-    cities_run = list(set(cities) - set(existing_cities['Entity'].values.tolist()))
+    condition_full = '[Source] = {0}'.format(get_sql_list(SITE))
+    condition_done = '[Timestamp] >= {0} AND '.format(get_sql_list(TIME['TODAY'])) + condition_full
+    entities_full = exist_db.get_logs(table_name=DB['LOG_TABLE_NAME'], condition=condition_full)
+    entities_done = exist_db.get_logs(table_name=DB['LOG_TABLE_NAME'], condition=condition_done)
+    entities_run = list(set(entities_full) - set(entities_done if entities_done else []))
 
-for city in cities_run:
-    city_object = Diandianzu(city)
-    city_object.run()
+for entity in entities_run:
+    entity_object = Diandianzu(entity)
+    entity_object.run()
 
     with Mssql(config=keys.dbconfig_mkt) as entity_db:
-        entity_db.upload(df=city_object.df, table_name=PATH['DETAIL_TABLE'], schema='CHN_MKT', new_id=SITE)
-        entity_db.upload(df=city_object.info, table_name=PATH['INFO_TABLE'], schema='CHN_MKT', new_id=SITE,
-                         dedupe_col='Source_ID')
-        entity_db.log(Entity=city, Timestamp=TIME['TODAY'], Source=SITE, start=1, end=len(city_object.info))
+        entity_db.upload(df=entity_object.df, table_name=DB['DETAIL_TABLE'], new_id=SITE)
+        entity_db.upload(df=entity_object.info, table_name=DB['INFO_TABLE'], new_id=SITE, dedupe_col='Source_ID')
+        entity_db.log(Entity=entity, Timestamp=TIME['TODAY'], Source=SITE, start=1, end=len(entity_object.info))
 
 scrapyemail = em.Email()
-scrapyemail.send(subject='[Scrapy] ' + PATH['DETAIL_TABLE'], content='Done', attachment=PATH['LOG_PATH'])
+scrapyemail.send(subject='[Scrapy] ' + DB['DETAIL_TABLE'], content='Done', attachment=PATH['LOG_PATH'])
 scrapyemail.close()
 exit(0)
