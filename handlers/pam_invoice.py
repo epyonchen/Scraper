ce35625ef827e4fb3784be55bd28c88408ee6c26
benson.chen <benson.chen@ap.jll.com>
2021-02-02 12:00:44 +0800

    Change irregular_tax.py to pam_invoice.py

--- handlers/pam_invoice.py
+++ handlers/pam_invoice.py
@@ -7,39 +7,26 @@ Created on May 11th 2019
 import re
 import requests
 import os
-import pandas as pd
 import handlers.pagemanipulate as pm
 import utils.utility_email as em
 from func_timeout import func_set_timeout
+from func_timeout.exceptions import FunctionTimedOut
 from PIL import Image
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from handlers.db import Mssql, get_sql_list
 from handlers.baidu_api import Baidu_ocr
 from utils.utility_log import get_logger
-from utils.utility_commons import PATH, TIME, DB, excel_to_df, df_to_excel, get_job_name
-import keys
-
-SITE = get_job_name()
-PATH['SCREENSHOT_PATH'] = PATH['PIC_DIR'] + r'\screen_shot.png'
-PATH['VCODE_PATH'] = PATH['PIC_DIR'] + r'\vcode.png'
-PATH['TAX_DETAIL_FILE'] = 'Irregular_Tax'
-PATH['TAX_FILE'] = 'Irregular_Tax_Summary'
-PATH['ATTACHMENT_FILE'] = '{0}_异常发票清单_{1}'
-PATH['LOG_PATH'] = PATH['LOG_DIR'] + '\\' + SITE + '.log'
+from utils.utility_commons import PATH, TIME, excel_to_df, df_to_excel
 
 
-DB['TAX_DETAIL_TABLE'] = 'Scrapy_' + SITE
-DB['TAX_TABLE'] = 'Scrapy_' + SITE + '_Summary'
-DB['ACCESS_TABLE'] = 'Scrapy_Irregular_Tax_Access'
 
 
 logger = get_logger(__name__)
 count = 0
 
 
-class Tax:
+class PAM_Invoice:
 
     def __init__(self, link, username, password):
 
@@ -50,6 +37,7 @@ class Tax:
         self.web.driver.implicitly_wait(10)
         self.session = requests.session()
         self.cookies = requests.cookies.RequestsCookieJar()
+        self.df = dict()
 
     # Crop validation code pic from screen shot
     def get_vcode_pic(self):
@@ -195,15 +183,14 @@ class Tax:
 
         while True:
             # Exit with error when login takes too much time
-            # try:
-            #     func_timeout(timeout=3600, func=self.login)
-
-            # except FunctionTimedOut as e:
-            #     logger.exception('Timeout. {0}'.format(e))
-            #     exit(1)
-            # except Exception as e:
-            #     logger.exception(e)
-            self.login()
+            try:
+                self.login()
+            except FunctionTimedOut as e:
+                logger.exception('Timeout. {0}'.format(e))
+                exit(1)
+            except Exception as e:
+                logger.exception(e)
+            # self.login()
 
             success = self.get()
             if success:
@@ -228,7 +215,7 @@ class Tax:
         return df, detail_df
 
 
-def _send_email(entity, receiver, attachment):
+def invoice_send_email(entity, receiver, attachment):
     # Send email
     scrapymail = em.Email()
 
@@ -247,61 +234,3 @@ def _send_email(entity, receiver, attachment):
 
     scrapymail.close()
 
-
-if __name__ == '__main__':
-
-    logger.info('---------------   Irregular tax ratio query.   ---------------')
-
-    with Mssql(keys.dbconfig) as exist_db:
-        access = exist_db.select(DB['ACCESS_TABLE'])
-        condition = '[Timestamp] >= {0} AND [Source] = {1}'. \
-            format(get_sql_list(TIME['TODAY']), get_sql_list(SITE))
-        entities = '\'' + '\', \''.join(list(access['Entity_Name'])) + '\''
-        logs = exist_db.select(table_name=DB['LOG_TABLE_NAME'], condition=condition)
-        # Exclude entities with logs in same day. If no logs, refresh table
-        if not logs.empty:
-            logger.info('Exclude existing entities and continue.')
-            access_run = access[-access['Entity_Name'].isin(logs['Entity'])]
-        else:
-            logger.info('Delete existing records and start a new query.')
-            exist_db.delete(table_name=DB['TAX_TABLE'])
-            exist_db.delete(table_name=DB['TAX_DETAIL_TABLE'])
-            access_run = access
-
-    # Core scraping process
-    for index, row in access_run.iterrows():
-        logger.info('---------------   Start new job. Entity: {} Server:{}    ---------------'.
-                    format(row['Entity_Name'], row['Server']))
-        one_entity = Tax(link=row['Link'], username=row['User_Name'], password=row['Password'])
-        tax_df, tax_detail_df = one_entity.run(entity=row['Entity_Name'], server=row['Server'])
-
-        # Upload to database
-        entity_db = Mssql(keys.dbconfig)
-        entity_db.upload(df=tax_df, table_name=DB['TAX_TABLE'])
-        entity_db.upload(df=tax_detail_df, table_name=DB['TAX_DETAIL_TABLE'])
-        entity_db.log(start=TIME['PRE3MONTH'], end=TIME['TODAY'], Timestamp=TIME['TIMESTAMP'], Source=SITE,
-                      Entity=row['Entity_Name'])
-        entity_db.close()
-
-    # Ensure failure of scraping process do not interrupt email and sp execution
-    with Mssql(keys.dbconfig) as execute_db:
-        # Update Irregular_Ind by executing stored procedure
-        execute_db.call_sp(sp='CHN.Irregular_Tax_Refresh', table_name=DB['TAX_DETAIL_TABLE'],
-                           table_name2=DB['TAX_TABLE'])
-        for index, row in access.iterrows():
-            # Get irregular record
-            att = execute_db.call_sp(sp='CHN.Irregular_Tax_ETL', output=True, table_name=DB['TAX_DETAIL_TABLE'],
-                                     entity_name=row['Entity_Name'])
-            numeric_col = ['金额', '单价', '税率', '税额']
-
-            if att is not False:
-                att[numeric_col] = att[numeric_col].apply(pd.to_numeric)
-
-            _send_email(entity=row['Entity_Name'], receiver=row['Email_List'], attachment=att)
-
-    # Send email summary
-    scrapyemail_summary = em.Email()
-    scrapyemail_summary.send('[Scrapy]' + SITE, 'Done', PATH['LOG_PATH'],
-                             receivers='benson.chen@ap.jll.com;helen.hu@ap.jll.com')
-    scrapyemail_summary.close()
-    exit(0)
