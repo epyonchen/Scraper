1db561ea63fd7e0771113d3b3dda91c19a0d4346
benson.chen <benson.chen@ap.jll.com>
2021-03-11 12:05:58 +0800

    Change path to absolute path of file-self's parent

--- handlers/handler_libs.py
+++ handlers/handler_libs.py
@@ -1,4 +1,4 @@
 import sys
 import os
-sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + os.path.sep + ".."))
 # sys.path.append(r'C:\Users\benson.chen\Credentials')
