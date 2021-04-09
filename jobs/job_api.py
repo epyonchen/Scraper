b77d2b192db98c6f858f5dfab9ba3bd0d8ce3e82
benson.chen <benson.chen@ap.jll.com>
2021-04-09 14:30:43 +0800

    Formatting

--- jobs/job_api.py
+++ jobs/job_api.py
@@ -11,27 +11,7 @@ from utils.utility_commons import excel_to_df, df_to_excel
 
 
 amap = Amap('text')
-# input_df = pd.DataFrame()
-# input_df = input_df.append([{'keywords': '广东省广州市天河区体育东路160号平安大厦18楼、20-26楼', 'city': '广州'},])#, 'types': '120000'
-#
-plist = ['ref_Branch', 'ref_Brand', 'ref_name', 'ref_address', 'ref_pname', 'ref_cityname', 'ref_adname', 'MapIT_lon',
-         'MapIT_lat']
-city = ['Guangzhou', 'Shanghai', 'Shenzhen', 'Beijing', 'Nanjing', 'Suzhou', 'Chengdu', 'Tianjin', 'Qingdao', 'Wuhan']
-result = pd.DataFrame(columns=plist)
-for c in city:
-    input_df = excel_to_df(file_name='map', sheet_name=c)
+input_df = excel_to_df(file_name='test', sheet_name='Sheet2')
+result = amap.query(input_df)
 
-    output_df = amap.query(input_df)
-    result = pd.concat([result, output_df[plist]])
-
-df_to_excel(df=result, file_name='map', sheet_name='result-address')
-
-
-# import pandas as pd
-#
-# df = pd.DataFrame()
-# df = df.append([{'q': '', 'from': 'cn', 'to': 'en'}], ignore_index=True)
-# print(df)
-# bm = Baidu_translate()
-# r = bm.query(df)
-# print(r)
+# df_to_excel(df=result, file_name='电商协会_整理版')
